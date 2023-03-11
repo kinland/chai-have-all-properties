b2b35f1b5e8de58c8c46209da66809ca664d5dc9
dyhork <16787581+dyhork@users.noreply.github.com>
2023-03-11 03:32:54 -0800

    Reformat (no manual changes)

--- lib/propertiesAssertion.ts
+++ lib/propertiesAssertion.ts
@@ -14,7 +14,6 @@
 
 import AssertionError from 'assertion-error';
 
-
 /**
  * ### .property(name[, val[, msg]])
  *
@@ -128,97 +127,97 @@ import AssertionError from 'assertion-error';
  */
 
 function assertProperty(name, val, msg) {
-  if (msg) flag(this, 'message', msg);
+    if (msg) flag(this, 'message', msg);
 
-  var isNested = flag(this, 'nested')
-    , isOwn = flag(this, 'own')
-    , flagMsg = flag(this, 'message')
-    , obj = flag(this, 'object')
-    , ssfi = flag(this, 'ssfi')
-    , nameType = typeof name;
+    var isNested = flag(this, 'nested')
+        , isOwn = flag(this, 'own')
+        , flagMsg = flag(this, 'message')
+        , obj = flag(this, 'object')
+        , ssfi = flag(this, 'ssfi')
+        , nameType = typeof name;
 
-  flagMsg = flagMsg ? flagMsg + ': ' : '';
+    flagMsg = flagMsg ? flagMsg + ': ' : '';
 
-  if (isNested) {
-    if (nameType !== 'string') {
-      throw new AssertionError(
-        flagMsg + 'the argument to property must be a string when using nested syntax',
-        undefined,
-        ssfi
-      );
+    if (isNested) {
+        if (nameType !== 'string') {
+            throw new AssertionError(
+                flagMsg + 'the argument to property must be a string when using nested syntax',
+                undefined,
+                ssfi
+            );
+        }
+    } else {
+        if (nameType !== 'string' && nameType !== 'number' && nameType !== 'symbol') {
+            throw new AssertionError(
+                flagMsg + 'the argument to property must be a string, number, or symbol',
+                undefined,
+                ssfi
+            );
+        }
     }
-  } else {
-    if (nameType !== 'string' && nameType !== 'number' && nameType !== 'symbol') {
-      throw new AssertionError(
-        flagMsg + 'the argument to property must be a string, number, or symbol',
-        undefined,
-        ssfi
-      );
+
+    if (isNested && isOwn) {
+        throw new AssertionError(
+            flagMsg + 'The "nested" and "own" flags cannot be combined.',
+            undefined,
+            ssfi
+        );
     }
-  }
 
-  if (isNested && isOwn) {
-    throw new AssertionError(
-      flagMsg + 'The "nested" and "own" flags cannot be combined.',
-      undefined,
-      ssfi
-    );
-  }
+    if (obj === null || obj === undefined) {
+        throw new AssertionError(
+            flagMsg + 'Target cannot be null or undefined.',
+            undefined,
+            ssfi
+        );
+    }
 
-  if (obj === null || obj === undefined) {
-    throw new AssertionError(
-      flagMsg + 'Target cannot be null or undefined.',
-      undefined,
-      ssfi
-    );
-  }
+    var isDeep = flag(this, 'deep')
+        , negate = flag(this, 'negate')
+        , pathInfo = isNested ? _.getPathInfo(obj, name) : null
+        , value = isNested ? pathInfo.value : obj[name];
 
-  var isDeep = flag(this, 'deep')
-    , negate = flag(this, 'negate')
-    , pathInfo = isNested ? _.getPathInfo(obj, name) : null
-    , value = isNested ? pathInfo.value : obj[name];
+    var descriptor = '';
+    if (isDeep) descriptor += 'deep ';
+    if (isOwn) descriptor += 'own ';
+    if (isNested) descriptor += 'nested ';
+    descriptor += 'property ';
 
-  var descriptor = '';
-  if (isDeep) descriptor += 'deep ';
-  if (isOwn) descriptor += 'own ';
-  if (isNested) descriptor += 'nested ';
-  descriptor += 'property ';
+    var hasProperty;
+    if (isOwn) hasProperty = Object.prototype.hasOwnProperty.call(obj, name);
+    else if (isNested) hasProperty = pathInfo.exists;
+    else hasProperty = _.hasProperty(obj, name);
 
-  var hasProperty;
-  if (isOwn) hasProperty = Object.prototype.hasOwnProperty.call(obj, name);
-  else if (isNested) hasProperty = pathInfo.exists;
-  else hasProperty = _.hasProperty(obj, name);
+    // When performing a negated assertion for both name and val, merely having
+    // a property with the given name isn't enough to cause the assertion to
+    // fail. It must both have a property with the given name, and the value of
+    // that property must equal the given val. Therefore, skip this assertion in
+    // favor of the next.
+    if (!negate || arguments.length === 1) {
+        this.assert(
+            hasProperty
+            , 'expected #{this} to have ' + descriptor + _.inspect(name)
+            , 'expected #{this} to not have ' + descriptor + _.inspect(name));
+    }
 
-  // When performing a negated assertion for both name and val, merely having
-  // a property with the given name isn't enough to cause the assertion to
-  // fail. It must both have a property with the given name, and the value of
-  // that property must equal the given val. Therefore, skip this assertion in
-  // favor of the next.
-  if (!negate || arguments.length === 1) {
-    this.assert(
-      hasProperty
-      , 'expected #{this} to have ' + descriptor + _.inspect(name)
-      , 'expected #{this} to not have ' + descriptor + _.inspect(name));
-  }
+    if (arguments.length > 1) {
+        this.assert(
+            hasProperty && (isDeep ? _.eql(val, value) : val === value)
+            , 'expected #{this} to have ' + descriptor + _.inspect(name) + ' of #{exp}, but got #{act}'
+            , 'expected #{this} to not have ' + descriptor + _.inspect(name) + ' of #{act}'
+            , val
+            , value
+        );
+    }
 
-  if (arguments.length > 1) {
-    this.assert(
-      hasProperty && (isDeep ? _.eql(val, value) : val === value)
-      , 'expected #{this} to have ' + descriptor + _.inspect(name) + ' of #{exp}, but got #{act}'
-      , 'expected #{this} to not have ' + descriptor + _.inspect(name) + ' of #{act}'
-      , val
-      , value
-    );
-  }
-
-  flag(this, 'object', value);
+    flag(this, 'object', value);
 }
 
 Assertion.addMethod('property', assertProperty);
 
 function assertOwnProperty(name, value, msg) {
-  flag(this, 'own', true);
-  assertProperty.apply(this, arguments);
+    flag(this, 'own', true);
+    assertProperty.apply(this, arguments);
 }
 
 Assertion.addMethod('ownProperty', assertOwnProperty);
